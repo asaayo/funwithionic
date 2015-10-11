ac968729f794f36bba53b7285263261629a4c28a
asaayo <brandon.trumble@gmail.com>
2015-10-11 12:39:58 -0700

    List showing up, yay!

--- www/js/app.js
+++ www/js/app.js
@@ -3,10 +3,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'controllerThingy'])
-	.run(function ($ionicPlatform){
-		
-	})
+angular.module('starter', ['ionic', 'starter.controllers'])
+	
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,6 +16,5 @@ angular.module('starter', ['ionic', 'controllerThingy'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-	}
   });
 })
